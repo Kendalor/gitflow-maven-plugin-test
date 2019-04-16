88691f3408b005f43080c35ad917cf22797fad4e
Patrick Rehn <patrickr@inhouse.nterra.com>
2019-04-16 09:53:27 +0200

    stuff

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -12,15 +12,20 @@ echo 'The following complex command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
 NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
+echo "Name"
+echo $NAME
 set +x
 
 echo 'The following complex command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
 VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
+echo "Version"
+echo $VERSION
 set +x
 
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
 java -jar target/${NAME}-${VERSION}.jar
+echo "Coppied Files to target/$(NAME)-$(VERSION).jar"
